1921a9047388b7039613324ce59594664a42c0f2
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2022-06-02 23:54:33 -0700

    Rewrites onet-page function to use plist args

--- lib/build-site.el
+++ lib/build-site.el
@@ -28,126 +28,133 @@
 (defvar onet-template-footer "lib/templates/footer.html"
   "Path, relative to `onet-base-bath' to footer template file.")
 
-(defun onet-page (title input output &optional nav)
-  "Build webpage from INPUT to OUTPUT using TITLE.
-If NAV is non-nil, omit the navigation template."
-  (with-temp-file (concat onet-base-path output)
-    ;; This has to be backwards (bottom-up). Probably because the point stays
-    ;; at zero, and `insert-file-contents' always inserts content after the
-    ;; point.
-    (insert-file-contents (concat onet-base-path onet-template-footer))
-    (insert-file-contents (concat onet-base-path input))
-    (unless nav
-      (insert-file-contents (concat onet-base-path onet-template-nav)))
-    (insert-file-contents (concat onet-base-path onet-template-header))
-    (while (re-search-forward "{{TITLE}}" nil t)
-      (replace-match title t))))
+(defun onet-page (&rest args)
+  "Function to build pages for olivertaylor.net
+:title will be used to create HTML title.
+:filename is basename of file.
+:section is optional, name of dir in root.
+:omit-nav, when non-nil, omits navigation."
+  (let* ((title (plist-get args :title))
+         (section (plist-get args :section))
+         (filename (plist-get args :filename))
+         (omit-nav (plist-get args :omit-nav))
+         (output (concat onet-base-path section "/" filename))
+         (input (concat onet-base-path "lib/" section "/" filename))
+         (footer (concat onet-base-path onet-template-footer))
+         (nav (concat onet-base-path onet-template-nav))
+         (header (concat onet-base-path onet-template-header)))
+    (with-temp-file output
+      ;; The page is built bottom-up, so footer first.
+      (insert-file-contents footer)
+      (insert-file-contents input)
+      (unless omit-nav (insert-file-contents nav))
+      (insert-file-contents header)
+      (while (re-search-forward "{{TITLE}}" nil t)
+        (replace-match title t)))))
 
 ;;; Pages
 
-(onet-page "Home"
-           "lib/index.html"
-           "index.html"
-           t)
+(onet-page :title "Home"
+           :filename "index.html"
+           :omit-nav t)
 
-(onet-page "Not Found!"
-           "lib/404.html"
-           "404.html")
+(onet-page :title "Not Found!"
+           :filename "404.html")
 
 ;;; Work Pages
 
-(onet-page "Engineering"
-           "lib/work/engineering.html"
-           "work/engineering.html")
+(onet-page :title "Engineering"
+           :filename "engineering.html"
+           :section "work")
 
-(onet-page "Thoughts on Growing a Company"
-           "lib/work/growth-thoughts.html"
-           "work/growth-thoughts.html")
+(onet-page :title "Thoughts on Growing a Company"
+           :filename "growth-thoughts.html"
+           :section "work")
 
-(onet-page "Notes on Managing"
-           "lib/work/managing.html"
-           "work/managing.html")
+(onet-page :title "Notes on Managing"
+           :filename "managing.html"
+           :section "work")
 
-(onet-page "My Productivity System"
-           "lib/work/productivity.html"
-           "work/productivity.html")
+(onet-page :title "My Productivity System"
+           :filename "productivity.html"
+           :section "work")
 
 ;;; Notes Pages
 
-(onet-page "A Partial List of Movies Released in 2007"
-           "lib/notes/20161218_2007_movies.html"
-           "notes/20161218_2007_movies.html")
+(onet-page :title "A Partial List of Movies Released in 2007"
+           :filename "20161218_2007_movies.html"
+           :section "notes")
 
-(onet-page "Harrison Ford Movies, 1980-2000"
-           "lib/notes/20161218_harrison_ford_movies.html"
-           "notes/20161218_harrison_ford_movies.html")
+(onet-page :title "Harrison Ford Movies, 1980-2000"
+           :filename "20161218_harrison_ford_movies.html"
+           :section "notes")
 
-(onet-page "Software I Love"
-           "lib/notes/20161218_software_love.html"
-           "notes/20161218_software_love.html")
+(onet-page :title "Software I Love"
+           :filename "20161218_software_love.html"
+           :section "notes")
 
-(onet-page "Worth the Money"
-           "lib/notes/20200330_worth_the_money.html"
-           "notes/20200330_worth_the_money.html")
+(onet-page :title "Worth the Money"
+           :filename "20200330_worth_the_money.html"
+           :section "notes")
 
-(onet-page "Recommended Books"
-           "lib/notes/20200531_recommended_books.html"
-           "notes/20200531_recommended_books.html")
+(onet-page :title "Recommended Books"
+           :filename "20200531_recommended_books.html"
+           :section "notes")
 
-(onet-page "Perfection"
-           "lib/notes/perfection.html"
-           "notes/perfection.html")
+(onet-page :title "Perfection"
+           :filename "perfection.html"
+           :section "notes")
 
-(onet-page "Structured to Innovate"
-           "lib/notes/structured-to-innovate.html"
-           "notes/structured-to-innovate.html")
+(onet-page :title "Structured to Innovate"
+           :filename "structured-to-innovate.html"
+           :section "notes")
 
-(onet-page "Writing in Plain Text"
-           "lib/notes/writing_in_plain_text.html"
-           "notes/writing_in_plain_text.html")
+(onet-page :title "Writing in Plain Text"
+           :filename "writing_in_plain_text.html"
+           :section "notes")
 
-(onet-page "Revelation of the Complex"
-           "lib/notes/20130908_revelation_complex.html"
-           "notes/20130908_revelation_complex.html")
+(onet-page :title "Revelation of the Complex"
+           :filename "20130908_revelation_complex.html"
+           :section "notes")
 
 ;;; Emacs Pages
 
-(onet-page "Emacs Keybindings That Won't Get Overridden by Minor Modes"
-           "lib/emacs/bosskey-mode.html"
-           "emacs/bosskey-mode.html")
+(onet-page :title "Emacs Keybindings That Won't Get Overridden by Minor Modes"
+           :filename "bosskey-mode.html"
+           :section "emacs")
 
-(onet-page "Confirm Killing Modified Buffers"
-           "lib/emacs/buffer-confirm-kill.html"
-           "emacs/buffer-confirm-kill.html")
+(onet-page :title "Confirm Killing Modified Buffers"
+           :filename "buffer-confirm-kill.html"
+           :section "emacs")
 
-(onet-page "Custom Scratch Buffers"
-           "lib/emacs/custom-scratch-buffers.html"
-           "emacs/custom-scratch-buffers.html")
+(onet-page :title "Custom Scratch Buffers"
+           :filename "custom-scratch-buffers.html"
+           :section "emacs")
 
-(onet-page "Extending Emacs Bookmarks to Work With EWW"
-           "lib/emacs/extending-emacs-bookmarks.html"
-           "emacs/extending-emacs-bookmarks.html")
+(onet-page :title "Extending Emacs Bookmarks to Work With EWW"
+           :filename "extending-emacs-bookmarks.html"
+           :section "emacs")
 
-(onet-page "Keymap Prompts"
-           "lib/emacs/keymap-prompt.html"
-           "emacs/keymap-prompt.html")
+(onet-page :title "Keymap Prompts"
+           :filename "keymap-prompt.html"
+           :section "emacs")
 
-(onet-page "Kill Buffer DWIM"
-           "lib/emacs/kill-buffer-dwim.html"
-           "emacs/kill-buffer-dwim.html")
+(onet-page :title "Kill Buffer DWIM"
+           :filename "kill-buffer-dwim.html"
+           :section "emacs")
 
-(onet-page "Mark Line and Marking Transient Command"
-           "lib/emacs/mark-line-transient-command.html"
-           "emacs/mark-line-transient-command.html")
+(onet-page :title "Mark Line and Marking Transient Command"
+           :filename "mark-line-transient-command.html"
+           :section "emacs")
 
-(onet-page "Splitting Windows With the Mouse"
-           "lib/emacs/mouse-window-splits.html"
-           "emacs/mouse-window-splits.html")
+(onet-page :title "Splitting Windows With the Mouse"
+           :filename "mouse-window-splits.html"
+           :section "emacs")
 
-(onet-page "How Package.el Works with Use Package"
-           "lib/emacs/notes-on-package-el.html"
-           "emacs/notes-on-package-el.html")
+(onet-page :title "How Package.el Works with Use Package"
+           :filename "notes-on-package-el.html"
+           :section "emacs")
 
-(onet-page "Quick Help: Emacs as a Text Productivity Platform"
-           "lib/emacs/quick-help.html"
-           "emacs/quick-help.html")
+(onet-page :title "Quick Help: Emacs as a Text Productivity Platform"
+           :filename "quick-help.html"
+           :section "emacs")
